a73e0dd8d5fa867ded8938083c5567ca88b52e98
Viviana Benitez <viviana.bernitez@gmail.com>
2023-07-25 12:31:37 -0500

    added response in the controller

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,6 +1,8 @@
 const text = async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
+    console.log(text);
+    res.status(200).json({ text });
   } catch (error) {
     console.log("Error", error);
     res.status(500).json({ error: error.message });
