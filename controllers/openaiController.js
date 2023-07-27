ab059083dec812c7a3d2de488db6283baaf492b3
Viviana Benitez <viviana.bernitez@gmail.com>
2023-07-27 11:00:18 -0500

    added connection with GPT, created assist and code controllers with the specific content for the system role

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,63 @@
+import { openai } from "../index.js";
+
 const text = async (req, res) => {
   try {
     const { text, activeChatId } = req.body;
-    console.log(text);
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: text}],
+    });
+    console.log(text)
+    console.log(response.data.choices[0].message);
+    res.status(200).json({ text });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const code = async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an assistant coder who responds with only code and no explanations.",
+        },
+        { role: "user", content: text },
+      ],
+    });
+
+    console.log(text, response);
+    res.status(200).json({ text });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const assist = async (req, res) => {
+  try {
+    const { text, activeChatId } = req.body;
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful assistant that serves to only complete user's thoughts or sentences.",
+        },
+        { role: "user", content: `Finish my thought: ${text}` },
+      ],
+    });
+
+    console.log(text, response);
     res.status(200).json({ text });
   } catch (error) {
     console.log("Error", error);
